4db8cf2e71376473170716c8fb2cc91802f0e8dc
AdrianMQ <steep.may.dld@gmail.com>
2022-06-23 11:18:16 -0500

    1 boton

--- PrimeraAplicacion/Form1.Designer.cs
+++ PrimeraAplicacion/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -100,11 +101,21 @@
             this.label7.TabIndex = 6;
             this.label7.Text = "label7";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(361, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(540, 270);
+            this.ClientSize = new System.Drawing.Size(540, 294);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -128,6 +139,7 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
     }
 }
 
